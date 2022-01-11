cc93963c4812ad0e85063ed30f2f72960fb8cdd4
EC2 Default User <ec2-user@ip-172-31-84-239.ec2.internal>
2022-01-11 12:04:13 +0000

    [bookers3]Finish2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  #get 'users/index'
+  #get 'users/show'
+  #get 'users/new'
+  #get 'users/edit'
+  #get 'books/index'
+  #get 'books/show'
+  #get 'books/new'
+  #get 'books/edit'
+  devise_for :users
+  root :to => 'homes#top'
+  get 'homes/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :books
+  resources :users
+
 end
